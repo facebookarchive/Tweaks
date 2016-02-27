9c8e406aeeda07c2652efd33706af46940998a78
Callum Boddy <callum.boddy@gmail.com>
2016-02-27 15:33:14 +0000

    code review

--- FBTweak/_FBTweakColorViewControllerHexDataSource.h
+++ FBTweak/_FBTweakColorViewControllerHexDataSource.h
@@ -1,10 +1,11 @@
-//
-//  _FBTweakColorViewControllerHexDataSource.h
-//  FBTweak
-//
-//  Created by Callum Boddy on 25/02/2016.
-//  Copyright © 2016 Facebook. All rights reserved.
-//
+/**
+ Copyright (c) 2014-present, Facebook, Inc.
+ All rights reserved.
+ 
+ This source code is licensed under the BSD-style license found in the
+ LICENSE file in the root directory of this source tree. An additional grant
+ of patent rights can be found in the PATENTS file in the same directory.
+ */
 
 #import <Foundation/Foundation.h>
 #import "_FBTweakColorViewControllerDataSource.h"
